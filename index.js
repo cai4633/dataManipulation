a93b092ab53e650164f3414022f6e4cd36215e6b
cai4633 <463353522@qq.com>
2018-07-14 17:22:08 +0800

    v1.0.0 练习使用vue创建简单的页面；

---
+++ index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue';
+var app1=new Vue({
+	el:'#app1',
+	data:{
+		name:'',
+		age:'',
+		des:'',
+		item:{
+			name:'姓名：',
+			age:'年龄：',
+			des:'简介：'
+		}
+	},
+	methods:{
+		clearIfo:function(){
+			this.name='';	
+			this.age='';
+			this.des='';
+		}
+	}
+});
+
